fd553510ec46733b3a07c09ea81d95c2547495ad
Leonardo Farias <leo.farias.05@gmail.com>
2017-03-07 16:34:10 -0300

    Estilizando Destaques

---
+++ index.php
@@ -0,0 +1,159 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.css">
+    
+    <link rel="stylesheet" href="css/style.css">
+    
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+      
+  </head>
+  <body>
+  <header class="header">
+  
+        <nav class="navbar fixed-top navbar-toggleable-md navbar-light">
+          <div class="container">
+          <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#menuprincipal" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="fa fa-bars" aria-hidden="true""></span>
+          </button>
+          <a class="navbar-brand" href="#"><img src="imagens/logobranca.png"></a>
+
+          <div class="collapse navbar-collapse" id="menuprincipal">
+            <ul class="navbar-nav ml-auto mt-2 mt-md-0">
+              <li class="nav-item pr-2 active">
+                <a class="nav-link" href="#">Saae</a>
+              </li>
+              <li class="nav-item pr-2">
+                <a class="nav-link" href="#">Agencia Virtual</a>
+              </li>
+              <li class="nav-item pr-2">
+                <a class="nav-link" href="#">Transparencias</a>
+              </li>
+              <li class="nav-item pr-2">
+                <a class="nav-link" href="#">Imprensa</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Ouvidoria</a>
+              </li>
+            </ul>
+          </div>
+          </div>
+        </nav>
+
+  </header>
+
+  <section class="slider">
+  <div class="container">
+      <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner" role="listbox">
+          
+         <div class="carousel-item active">
+            <img src="https://placehold.it/1200x200" alt="...">
+            <div class="carousel-caption d-none d-md-block">
+              <h3>...</h3>
+              <p>...</p>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <img src="https://placehold.it/1200x200" alt="...">
+            <div class="carousel-caption d-none d-md-block">
+              <h3>...</h3>
+              <p>...</p>
+            </div>
+          </div> 
+           <div class="carousel-item">
+            <img src="https://placehold.it/1200x200" alt="...">
+            <div class="carousel-caption d-none d-md-block">
+              <h3>...</h3>
+              <p>...</p>
+            </div>
+          </div> 
+          </div>
+        <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+      </div>
+    </div>
+  </section>
+
+  <section class="noticias-title mt-5">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-8">
+          <h1 class="h3">Destaques Saae</h1>
+        </div>
+        <div class="col-md-4">
+          <h1 class="h3">Video</h1>
+        </div>
+      </div>
+
+   
+  
+
+
+      <div class="row">
+     
+        <div class="col-md-3">
+               
+                <div class="card ">
+                <img class="card-img-top" src="https://placehold.it/318x200" class="card-img-top" alt="Card image cap">
+                <div class="card-block">
+                  <h4 class="card-title">Card title</h4>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            
+                </div>
+                </div>
+
+               <div class="col-md-3">
+               
+                <div class="card ">
+                <img class="card-img-top" src="https://placehold.it/318x200" class="card-img-top" alt="Card image cap">
+                <div class="card-block">
+                  <h4 class="card-title">Card title</h4>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            
+                </div>
+                </div>
+               
+         
+            
+      
+             <div class="col-md-3">
+               
+                <div class="card ">
+                <img class="card-img-top" src="https://placehold.it/318x200" class="card-img-top" alt="Card image cap">
+                <div class="card-block">
+                  <h4 class="card-title">Card title</h4>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            
+                </div>
+                </div>               
+           
+
+      </div>
+
+      </div>
+  </section>
+ 
+    <!-- jQuery first, then Tether, then Bootstrap JS. -->
+    <script src="js/jquery-3.1.1.slim.min.js"></script>
+    <script src="js/tether.min.js" ></script>
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
